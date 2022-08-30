1c4ec2a6151b920de7b6454122ee28aa1be95e4b
simone ricciardo <you@example.com>
2022-08-30 17:32:00 +0200

    primo layout

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="title">
+            <h1>BOOLFLIX</h1>
+        </div>
+        <div>
+            <input type="text" v-model="searchMovie" placeholder="Cerca" >
+            <button>Cerca</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHeader',
+    data(){
+        return{
+            searchMovie: ''
+        }
+    },
+}
+</script>
+
+<style lang="scss">
+
+.container{
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 25px;
+    background-color: #000;
+    height: 70px;
+
+    .title{
+        color: rgb(227, 0, 0);
+    }
+}
+
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container-main">
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>    
+            <ul>
+                <li v-for="info in MovieList" :key="info.id" :info="info">{{info.title}}</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'AppMain',
+
+    data() {
+        return {
+            MovieList: [],
+        }
+    },
+    created() {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=b897f127a3dbd85d52cd82ed32fa6440&query=2001&language=it-IT')
+        .then((results) => {
+          this.MovieList = results.data.results;
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+    }    
+}
+</script>
+
+<style lang="scss">
+
+</style>
